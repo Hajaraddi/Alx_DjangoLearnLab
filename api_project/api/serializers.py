aca1fb5b36da69d958d5ebcec52177e76e973418
Hajaraddi <addihajar5@gmail.com>
2025-08-01 18:41:13 +0100

    Implementing CRUD Operations

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        ModuleNotFoundErrorfields = '__all__'
+        fields = '__all__'
