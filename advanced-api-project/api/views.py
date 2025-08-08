3cad5fd644fb56659024beed1e736202b523f799
Hajaraddi <addihajar5@gmail.com>
2025-08-08 17:06:23 +0100

    Writing Unit tests for django rest framework APIs

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+
 from .models import Book
 from .serializers import BookSerializer
 
@@ -10,6 +12,22 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    
+    # Add filtering, searching, ordering
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    # Fields for filtering (exact match)
+    filterset_fields = ['title', 'author__name', 'publication_year']
+
+    # Fields for searching (text search)
+    search_fields = ['title', 'author__name']
+
+    # Fields for ordering
+    ordering_fields = ['title', 'publication_year']
+
+    # Optional: default ordering
+    ordering = ['title']
 
 #DetailView: katseft book whd b id
 class BookDetailView(generics.RetrieveAPIView):
