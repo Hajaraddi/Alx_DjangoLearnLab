91acfa1ad377c8039338dff8df663a5b75c083fe
Hajaraddi <addihajar5@gmail.com>
2025-08-14 17:45:31 +0100

    fix Comment

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -16,3 +16,14 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk':self.pk})
+    
+class Comment(models.Model):
+    post = models.ForeignKey('POST', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  
+    
+    def __str__(self):
+        return f"Comment by {self.author} on {self.author} on {self.post}"
+    
