6942edf9710f0351af32f200902772782af31be7
Hajaraddi <addihajar5@gmail.com>
2025-08-19 16:42:33 +0100

    push the folder to github again

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework import permissions
 
 # Create your views here.
 
@@ -11,4 +12,5 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
-    serializer_class = BookSerializer   
+    serializer_class = BookSerializer 
+    permission_classes = [permissions.IsAuthenticated]  
