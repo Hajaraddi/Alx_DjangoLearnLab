594fb6a74da83cdd6faa83eb3bc67351e7268fd3
Hajaraddi <addihajar5@gmail.com>
2025-08-22 13:40:18 +0100

    Add rest_framework

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from .models import User
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
